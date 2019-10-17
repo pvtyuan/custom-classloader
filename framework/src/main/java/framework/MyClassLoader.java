b6529bd81b2198f9a0881f07c2791a024814aed2
pvtyuan <pvtyuan@foxmail.com>
2019-10-17 17:57:04 +0800

    add getClassInputStream

--- framework/src/main/java/framework/MyClassLoader.java
+++ framework/src/main/java/framework/MyClassLoader.java
@@ -1,7 +1,34 @@
 package framework;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * Created by pvtyuan on 2019/10/17.
  */
-public class MyClassLoader {
+public class MyClassLoader extends URLClassLoader {
+    public MyClassLoader(URL[] urls) {
+        super(urls, null);
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        try {
+            InputStream inputStream = getClassInputStream(name);
+            if (inputStream == null) {
+                return super.loadClass(name);
+            }
+            byte [] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException("Class " + name + " not found");
+        }
+    }
+
+    private InputStream getClassInputStream(String name) {
+        return null;
+    }
 }
